6a5317ec90489bb29b2a0333bb2ad83834ea6a9f
Anushka Rai <anushka010mca23@igdtuw.ac.in>
2024-08-24 17:46:54 +0530

    Modified app.py in order to update nltk

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+# Ensure NLTK resources are downloaded
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+# Initialize the Porter Stemmer
 ps = PorterStemmer()
 
 
@@ -26,7 +28,7 @@ def transform_text(text):
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
 
-    text = y[:]  # stemming
+    text = y[:]  # Stemming
     y.clear()
 
     for i in text:
@@ -35,17 +37,17 @@ def transform_text(text):
     return " ".join(y)
 
 
+# Load the pre-trained models
 tfidf = pickle.load(open('vectorizer (1).pkl', 'rb'))
 model = pickle.load(open('model (1).pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
-if st.button ('Predict'):
-
-    # 1. preprocess
+if st.button('Predict'):
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
     # 3. Predict
     result = model.predict(vector_input)[0]
